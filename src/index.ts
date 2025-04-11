863eead6f2983b43af40d6b606259941f8726f39
Alphonso77 <762224+alphonso77@users.noreply.github.com>
2025-04-11 09:41:23 -0400

    stub out menu with operations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,45 @@
-export { RriCli } from './rri-cli'
+import { select, Separator } from '@inquirer/prompts';
+import { RriCli } from './rri-cli';
+
+async function run() {
+
+    try {
+        const answer = await select({
+            message: 'Choose a template to create:',
+            choices: [
+                { name: 'API', value: 'api', description: 'Generic crud operations for integrating with an external system' },
+                { name: 'Microservice', value: 'microservice', description: 'Lightweight NodeJs program to separate concerns' },
+                { name: 'NPM Package', value: 'npm-package', description: 'Abstract away services to be consumed by other packages' },
+                { name: 'Exit', value: 'exit', description: 'Exit the program' },
+                new Separator()
+            ],
+        });
+        console.log('You selected:', answer);
+
+        switch (answer) {
+            case "api":
+                await RriCli.createApiTemplate();
+                run();
+                break;
+            case "microservice": 
+                await RriCli.createMicroserviceTemplate();
+                run();
+                break;
+            case "npm-package":
+                await RriCli.createNpmPackageTemplate();
+                run();
+                break;
+            case "exit":
+                process.exit();
+                break;
+
+        }
+
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+
+}
+
+
+run();
--- src/rri-cli.ts
+++ src/rri-cli.ts
@@ -2,4 +2,16 @@ export class RriCli {
     printMsg = function() {
         console.log("this is a message from the demo package");
     }
+
+    static createApiTemplate = async function()  {
+        console.log('creating an api template');
+    }
+
+    static createMicroserviceTemplate = async function() {
+        console.log('creating a microservice template');
+    }
+
+    static createNpmPackageTemplate = async function () {
+        console.log('creating an npm package template');
+    }
 }
